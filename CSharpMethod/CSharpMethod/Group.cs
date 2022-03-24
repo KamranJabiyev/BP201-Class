c5cc131f5a9352ce8233ca30dbd9990ba854c7a0
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2022-03-24 17:22:44 +0400

    Add files via upload

---
+++ CSharpMethod/CSharpMethod/Group.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CSharpMethod
+{
+    class Group
+    {
+        public string groupName;
+        public int maxStuCount;
+
+        public string Info()
+        {
+            return $"Group : {groupName}, count : {maxStuCount}";
+        }
+    }
+}
---
+++ CSharpMethod/CSharpMethod/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpMethod
+{
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+
+        //constructor - 
+        public Student():this("Ibrahim","Isgenderov")
+        {
+            
+            Console.WriteLine("Student created");
+        }
+
+        public Student(string name,string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+        public Student(string name,string surname,int age):this(name,surname)
+        {
+            Age = age;
+        }
+        public void Detail()
+        {
+            Console.WriteLine($"{Name} {Surname} {Age}");
+        }
+    }
+}
